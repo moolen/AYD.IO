6ef8edea7b15a06d49ffc32eafd6be46f6733d39
Moritz Beller <beller.moritz@googlemail.com>
2013-09-11 21:28:35 +0200

    adding ping stuff....

--- reciever/app.js
+++ reciever/app.js
@@ -45,7 +45,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.post('/', routes.index);
+app.get('/ping', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
